e4edb9e87585fef821730b3e508c3c350fd5f18b
Shiming Zhang <wzshiming@foxmail.com>
2021-08-16 20:41:26 +0800

    Fix in windows

--- command_windows.go
+++ command_windows.go
@@ -7,12 +7,12 @@ import (
 	"syscall"
 )
 
-func command(name string, arg ...string) (string, error) {
+func command(name string, arg ...string) error {
 	c := exec.Command(name, arg...)
 	c.SysProcAttr = &syscall.SysProcAttr{HideWindow: true}
 	out, err := c.CombinedOutput()
 	if err != nil {
-		return "", fmt.Errorf("%q: %w: %q", strings.Join(append([]string{name}, arg...), " "), err, out)
+		return fmt.Errorf("%q: %w: %q", strings.Join(append([]string{name}, arg...), " "), err, out)
 	}
-	return strings.TrimSpace(string(out)), nil
+	return nil
 }
